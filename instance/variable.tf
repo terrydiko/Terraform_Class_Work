b5b50531a9546137a7db721ef578633e39e3a67b
malcnwai <mnwainokpor200@gmail.com>
2021-12-22 23:10:00 -0500

    tf-local

--- instance/variable.tf
+++ instance/variable.tf
@@ -1,19 +1,49 @@
 variable "ami_id" {
+    type = map(string)
+    default = {
+        "prd" = "ami-0c2b8ca1dad447f8a"
+        "stg" = "ami-0c2b8ca1dad447f8a"
+        "dev" = "ami-0ec1545979d0dc885" #us-east-1 Rocky 8 Linux
+    }
 
 }
  variable "region" {
+     type = string
+     default = "us-east-1"
 
 
  }
 
  variable "type_instance" {
+     type = map(string)
+     default = {
+       "prd" = "t3.medium"
+       "dev" = "t2.micro"
+       "stg" = "t2.micro"
+     }
+     
 
  }
 
  variable "terraform_Env" {
+     type = string
 
  }
-variable "manatory_tags" {
+variable "mandatory_tags" {
+    type = map(map(string))
+    default = {
+      "prd" = {
+        "Env" = "prd"
+      }
+
+      "stg" = {
+          "Env" = "stg"
+      }
+      
+      "dev" = {
+          "Env" = "dev"
+      }
+    }
 
 
 }
