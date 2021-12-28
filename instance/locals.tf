74c8b51595c141aeacbf0ac66d90951cca025b4b
anyi1985 <nkuche.iheanyi@gmail.com>
2021-12-27 21:49:12 -0500

    updated modules and instance folders

---
+++ instance/locals.tf
@@ -0,0 +1,6 @@
+locals {
+  ami           = lookup(var.ami_id, var.terraform_Env)
+  region        = var.region
+  instance_type = lookup(var.type_instance, var.terraform_Env)
+  tags          = lookup(var.mandatory_tags, var.terraform_Env)
+}
--- instance/variable.tf
+++ instance/variable.tf
@@ -1,19 +1,44 @@
 variable "ami_id" {
+  type = map(string) # some cases you may see "map"  OR  map(any) - all are the same
+  default = {
+    "prd" = "ami-0c2b8ca1dad447f8a" #"key" = "value"
+    "stg" = "ami-0c2b8ca1dad447f8a"
+    "dev" = "ami-0ec1545979d0dc885" # us-east-1 Rocky 8 Linux
+  }
 
 }
- variable "region" {
+variable "region" {
+  type    = string
+  default = "us-east-1"
 
 
- }
+}
 
- variable "type_instance" {
+variable "type_instance" {
+  type = map(string)
+  default = {
+    "prd" = "t3.medium"
+    "stg" = "t2.micro"
+    "dev" = "t2.micro"
+  }
 
- }
+}
 
- variable "terraform_Env" {
+variable "terraform_Env" {
+  type = string
 
- }
-variable "manatory_tags" {
-
-
+}
+variable "mandatory_tags" {
+  type = map(map(string))
+  default = {
+    "prd" = {
+      "Env" = "prd"
+    }
+    "stg" = {
+      "Env" = "stg"
+    }
+    "dev" = {
+      "Env" = "dev"
+    }
+  }
 }
